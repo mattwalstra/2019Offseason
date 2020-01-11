1075d55ac799cee385162957be71cf49d99765a2
mattwalstra <20walstramatthew@student.schs.org>
2020-01-11 23:46:34 +0000

    fix?

--- zebROS_ws/src/plane_detection/src/plane_detection.cpp
+++ zebROS_ws/src/plane_detection/src/plane_detection.cpp
@@ -9,13 +9,14 @@
 #include <string>
 
 #include "sensor_msgs/PointCloud2.h"
-#include <pcl/point_types.h>
+
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/conversions.h>
 #include <pcl/common/common.h>
+#include <pcl/point_types.h>
 
 //#include <Eigen/Core>
 
